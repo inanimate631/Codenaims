c6554b1151a4f0ef3bc4346c04c4219c9650d497
Artem <inanmate631@gmail.com>
2023-07-27 17:10:33 +0300

    fix: fix userIp

--- server/server.js
+++ server/server.js
@@ -69,8 +69,7 @@ let teamColor;
 let isGameIsPause = false;
 
 io.on("connection", (socket) => {
-  //handshake.headers["x-forwarded-for"].split(", ")[0];
-  const userIp = socket.id
+  const userIp = socket.handshake.headers["x-forwarded-for"].split(", ")[0]
   console.log("User connected:", userIp);
 
   if (connectedUsers.length > 0) {
